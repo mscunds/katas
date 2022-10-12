48fcd9fd93d8c55d2a21bcfad3422ee786e74755
Mark Sudau <sudaum@users.noreply.github.com>
2022-10-12 23:50:10 +0200

    Merge pull request #7
    * mediator-5: remove old implementation and add new

--- designpattern/mediator/src/main/java/de/mscs/kata/designpattern/mediator/complex/domain2/service/Domain2Service.java
+++
@@ -1,7 +0,0 @@
-package de.mscs.kata.designpattern.mediator.complex.domain2.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class Domain2Service {
-}
--- designpattern/mediator/src/main/java/de/mscs/kata/designpattern/mediator/simple/mediator/Mediator.java
+++
@@ -1,6 +0,0 @@
-package de.mscs.kata.designpattern.mediator.simple.mediator;
-
-public interface Mediator {
-    void findOne();
-    void findAll();
-}
