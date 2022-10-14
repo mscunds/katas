37cb44f0d9a054c0d6a70e01b73725a674781d28
sudaum <mark.sudau@adesso.de>
2022-10-14 17:23:52 +0200

    refactor(smaller changes):

--- designpattern/decorator/src/main/java/de/mscs/kata/designpattern/decorator/DecoratorApplication.java
+++ designpattern/decorator/src/main/java/de/mscs/kata/designpattern/decorator/DecoratorApplication.java
@@ -16,39 +16,39 @@ import org.springframework.context.ConfigurableApplicationContext;
 @Slf4j
 public class DecoratorApplication {
 
-    public static void main (String[] args) {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(DecoratorApplication.class)
-                .web(WebApplicationType.NONE)
-                .allowCircularReferences(false)
-                .logStartupInfo(true)
-                .build().run(args);
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(DecoratorApplication.class)
+				.web(WebApplicationType.NONE)
+				.allowCircularReferences(false)
+				.logStartupInfo(true)
+				.build().run(args);
 
-        DecoratorApplication application = context.getBean(DecoratorApplication.class);
-        application.showWrapper();
-    }
+		DecoratorApplication application = context.getBean(DecoratorApplication.class);
+		application.showWrapper();
+	}
 
-    private void showWrapper() {
-        Instant now = Instant.now();
-        SimpleDecorator decorator = new SimpleDecorator(now.minus(5, ChronoUnit.DAYS),"Ein Titel");
+	private void showWrapper() {
+		Instant now = Instant.now();
+		SimpleDecorator decorator = new SimpleDecorator(now.minus(5, ChronoUnit.DAYS), "Ein Titel");
 
-        AgeDecorator ageDecorator = new AgeDecorator(decorator);
-        FinishedDecorator finishedDecorator = new FinishedDecorator(decorator);
-        log.info("Der Decorator {} ist {} Millis alt", ageDecorator.getTitle(), ageDecorator.getAge());
-        log.info("Der Decorator {} ist{}beendet,", finishedDecorator.getTitle(), finishedDecorator.isFinished() ?
-                                                                                   " " : " nicht ");
+		AgeDecorator ageDecorator = new AgeDecorator(decorator);
+		FinishedDecorator finishedDecorator = new FinishedDecorator(decorator);
+		log.info("Der Datencontainer ist \"{}\"", decorator);
+		log.info("Der AgeDecorator sagt: \"{}\" ist {} Tage alt", ageDecorator.getTitle(), ageDecorator.getAge().toDaysPart());
+		log.info("Der FinishedDecorator sagt: \"{}\" ist{}beendet,", finishedDecorator.getTitle(), finishedDecorator.isFinished() ?
+				" " : " nicht ");
 
-        try {
-            finishedDecorator.setFinishedAt(now);
-        } catch (UnsupportedOperationException uoe) {
-            log.info("Der Decorator {} erlaubt das Setzen des finished Dates nicht", finishedDecorator.getTitle());
-        }
+		try {
+			finishedDecorator.setFinishedAt(now);
+		}
+		catch(UnsupportedOperationException uoe) {
+			log.info("Der FinishedDecorator \"{}\" erlaubt das Setzen des finished Dates nicht", finishedDecorator.getTitle());
+		}
 
-        ageDecorator.setFinishedAt(now.minus(1, ChronoUnit.DAYS));
-        finishedDecorator = new FinishedDecorator(ageDecorator);
-        log.info("Der Decorator {} ist{}beendet,", finishedDecorator.getTitle(), finishedDecorator.isFinished() ? " "
-                                                                                                               : " "
-                + "nicht ");
+		ageDecorator.setFinishedAt(now.minus(1, ChronoUnit.DAYS));
+		finishedDecorator = new FinishedDecorator(ageDecorator);
+		log.info("Der FinishedDecorator sagt: \"{}\" ist{}beendet.", finishedDecorator.getTitle(),
+                finishedDecorator.isFinished() ? " " : " nicht ");
 
-
-    }
+	}
 }
--- designpattern/decorator/src/main/java/de/mscs/kata/designpattern/decorator/wrapper/SimpleDecorator.java
+++ designpattern/decorator/src/main/java/de/mscs/kata/designpattern/decorator/wrapper/SimpleDecorator.java
@@ -5,9 +5,11 @@ import java.time.Instant;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class SimpleDecorator implements Decorator {
     private final Instant createdAt;
     @Setter
